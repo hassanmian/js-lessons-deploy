96ebd40070598fc5a6a8223f78b13c3e68f8858c
Hassan Mian <hassanmian@hassans-imac.lan>
2020-09-29 10:54:38 +0200

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import {Switch, Route, Link} from 'react-router-dom'
 
 function App() {
   return (
     <div>
-     <h1>Hej!</h1>
+     <h1>Hej, världen!</h1>
+     <ul>
+       <li><Link to="/">Home</Link></li>
+       <li><Link to="/contact">Contact</Link></li>
+       <li><Link to="/chat">Chat</Link></li>
+       <li><Link to="/about">About</Link></li>
+     </ul>
+     <Route path="/contact">
+        <p>Contact</p>
+      </Route>
+      <Route path="/chat">
+        <p>Chat</p>
+      </Route>
+      <Route path="/about">
+        <p>About</p>
+      </Route>
     </div>
   );
 }
