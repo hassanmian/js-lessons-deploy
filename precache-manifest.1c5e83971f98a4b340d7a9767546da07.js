510159d20fa97342b5acd4a50f17d59c6954961b
Hassan Mian <hassanmian@hassans-imac.lan>
2020-09-29 09:37:20 +0200

    Updates

---
+++ precache-manifest.1c5e83971f98a4b340d7a9767546da07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acab69363367d4a27c9aa009ce7f4a05",
+    "url": "/js-lessons-deploy/index.html"
+  },
+  {
+    "revision": "11b73de11331c91bdfc3",
+    "url": "/js-lessons-deploy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3fe8ee4c9bb69616266e",
+    "url": "/js-lessons-deploy/static/js/2.b283872f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js-lessons-deploy/static/js/2.b283872f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b73de11331c91bdfc3",
+    "url": "/js-lessons-deploy/static/js/main.d01e29b6.chunk.js"
+  },
+  {
+    "revision": "2b3d6f1d43a1cc8534d4",
+    "url": "/js-lessons-deploy/static/js/runtime-main.1e281e79.js"
+  }
+]);
